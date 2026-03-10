03d5d5a0591a4e0d6b0aa51ce4939050319f2a14
FacundoAznaran <aznaranfacundo@gmail.com>
2026-03-10 02:57:10 -0300

    probando git2

--- teoria_1/ejercicio_9/Program.cs
+++ teoria_1/ejercicio_9/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese las palabras:");
+string st = Console.ReadLine();
+
+bool simetria = true;
+
+for(int i = 0; i < st.Length/2; i++)
+{
+    if(st[i] != st[st.Length - 1 - i])
+    {
+        simetria = false;
+    }
+}
+
+Console.WriteLine(simetria);
--- teoria_1/ejercicio_9/obj/Debug/net10.0/ejercicio_9.AssemblyInfo.cs
+++ teoria_1/ejercicio_9/obj/Debug/net10.0/ejercicio_9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio_9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8e8c9113cd2ddcd93a39d133f774319221c0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio_9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio_9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
