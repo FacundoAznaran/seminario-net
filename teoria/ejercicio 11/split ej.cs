089662ffed331783e932d5bb3c14c41f20914eb1
FacundoAznaran <fakuk18t@gmail.com>
2026-04-05 14:54:02 -0300

    elimino la carpeta duplicada

--- teoria/ejercicio 11/split ej.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-Console.WriteLine("ingrese un texto");
-string texto = Console.ReadLine();
-string[] resultado = texto.Split(" ");
-foreach (String x in resultado)
-{
-    Console.WriteLine(x);
-}
--- teoria/ejercicio 16/saludo por comando.cs
+++
@@ -1,9 +0,0 @@
-for(int i = 0; i < args.Length; i++)
-{
-    Console.WriteLine($"hola {args[i]}");
-}
-
-foreach(string x in args)
-{
-    Console.WriteLine($"hola {x}");
-}
